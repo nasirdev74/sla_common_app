9e1bb7972ababade5114467916bb7e1bf6ea8a79
Nasir <nasir.dev74@gmail.com>
2023-09-13 14:14:03 +0600

    urls exported

--- package/sla_common/lib/sla_common.dart
+++ package/sla_common/lib/sla_common.dart
@@ -4,6 +4,7 @@ export 'constants/constants.dart';
 export 'extensions/extensions.dart';
 export 'utils/utils.dart';
 export 'data/api_client.dart';
+export 'data/urls.dart';
 export 'enums/enums.dart';
 export 'models/models.dart';
 export 'settings/settings.dart';
