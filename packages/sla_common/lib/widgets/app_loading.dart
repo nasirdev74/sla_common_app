45b810fb76a9dc318ed849f2485488cdaf10a846
Nasir <nasir.dev74@gmail.com>
2023-09-18 15:35:17 +0600

    package named added on loading widget

--- packages/sla_common/lib/widgets/app_loading.dart
+++ packages/sla_common/lib/widgets/app_loading.dart
@@ -1,7 +1,6 @@
 import 'dart:ui';
-import 'package:lottie/lottie.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:sla_common/sla_common.dart';
 
 class AppLoading extends StatelessWidget {
   const AppLoading({Key? key, this.inProgress = false}) : super(key: key);
@@ -29,6 +28,7 @@ class AppLoading extends StatelessWidget {
               child: Center(
                 child: Lottie.asset(
                   "assets/animations/loading.json",
+                  package: slaCommon,
                   height: 140.w,
                   width: 140.w,
                 ),
